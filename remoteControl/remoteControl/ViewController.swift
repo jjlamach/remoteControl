ff8e3abb8f3a3ccbdaf51a9811417f27c5ee3c16
jlama94 <lamajulio17@gmail.com>
2019-02-05 18:01:34 -0600

    First commit

---
+++ remoteControl/remoteControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  remoteControl
+//
+//  Created by Julio on 2/5/19.
+//  Copyright © 2019 Julio Lama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
