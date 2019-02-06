68f7413bb3f60c9cf9533f3f5e69af0dc1d29de4
jlama94 <lamajulio17@gmail.com>
2019-02-05 21:10:00 -0600

    Buttons

--- remoteControl/remoteControl/ViewController.swift
+++ remoteControl/remoteControl/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // channels
+    @IBOutlet weak var Channels: UISegmentedControl!
+    
+   /* On/Off button */
+    @IBOutlet weak var power: UISwitch!
+    
+    /* Slider. */
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    /* Channel buttons. */
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
